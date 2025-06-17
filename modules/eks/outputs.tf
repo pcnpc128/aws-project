226f323ab7eeeed6eda0478c47a4ae659cd85ae4
pcnpc128 <quasar421@gmail.com>
2025-06-17 16:39:37 +0900

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,4 +1,5 @@
 output "cluster_name"     { value = module.eks.cluster_name }
 output "cluster_endpoint" { value = module.eks.cluster_endpoint }
 output "cluster_ca"       { value = module.eks.cluster_certificate_authority_data }
-
+output "oidc_provider_url" { value = module.eks.cluster_oidc_issuer_url }
+output "oidc_thumbprint" { value = module.eks.cluster_oidc_thumbprint }
