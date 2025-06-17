871757ad7602dfd218aaa4ab1e8aa0c851ff2507
pcnpc128 <quasar421@gmail.com>
2025-06-17 15:03:24 +0900

    Update main.tf

--- modules/eks-app/main.tf
+++ modules/eks-app/main.tf
@@ -7,6 +7,21 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.default.token
+  alias                  = "eks"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.default.token
+  }
+}
+
 resource "kubernetes_deployment" "app" {
   metadata {
     name      = var.app_name
