2d642b6de6a9ddb1d5e6c082f0cfe0793e9f15c0
pcnpc128 <quasar421@gmail.com>
2025-06-17 15:35:18 +0900

    Update variables.tf

--- modules/eks-app/variables.tf
+++ modules/eks-app/variables.tf
@@ -1,61 +1,64 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_endpoint" {
+  description = "EKS cluster API server endpoint"
+  type        = string
+}
+
+variable "cluster_ca" {
+  description = "Base64 encoded EKS cluster CA certificate"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region of the EKS cluster"
+  type        = string
+}
+
 variable "app_name" {
-  description = "앱 이름 및 레이블로 사용됨"
+  description = "Name of the application"
   type        = string
 }
 
 variable "namespace" {
-  description = "배포할 Kubernetes 네임스페이스"
+  description = "Kubernetes namespace to deploy into"
   type        = string
   default     = "default"
 }
 
 variable "replica_count" {
-  description = "Pod 수 (HPA 이전의 기본 값)"
+  description = "Number of replicas for the deployment"
   type        = number
   default     = 2
 }
 
 variable "app_image" {
-  description = "ECR에서 가져올 앱 컨테이너 이미지 경로"
+  description = "Docker image for the application"
   type        = string
 }
 
 variable "db_host" {
-  description = "접속할 DB 호스트 (예: rds.2whhosting.com)"
+  description = "Hostname of the database"
   type        = string
 }
 
 variable "db_port" {
-  description = "DB 포트 번호"
+  description = "Port number of the database"
   type        = string
   default     = "3306"
 }
 
 variable "container_port" {
-  description = "앱 컨테이너가 사용하는 포트"
+  description = "Port that the application container listens on"
   type        = number
   default     = 8080
 }
 
-variable "cluster_name" {
-  description = "EKS 클러스터 이름 (Cluster Autoscaler용)"
-  type        = string
+variable "service_port" {
+  description = "Port number the service exposes"
+  type        = number
+  default     = 80
 }
-
-variable "region" {
-  description = "EKS 클러스터가 위치한 AWS 리전"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "EKS 클러스터가 속한 VPC ID"
-  type        = string
-}
-
-variable "app_service_name" {
-  description = "Ingress에서 참조할 서비스 이름"
-  type        = string
-}
-
-variable "cluster_endpoint" {}
-variable "cluster_ca" {}
