a49cd98d9aac7291a0617bcf4ef3b78e2f2404d7
pcnpc128 <quasar421@gmail.com>
2025-06-17 16:29:34 +0900

    Create alb_irsa.tf

---
+++ modules/eks-app/alb_irsa.tf
@@ -0,0 +1,55 @@
+# IRSA: IAM Role for Service Account for AWS Load Balancer Controller
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url             = var.cluster_oidc_issuer_url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [var.cluster_oidc_thumbprint]
+}
+
+resource "aws_iam_role" "alb_controller_irsa" {
+  name = "alb-controller-irsa-${var.cluster_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "${replace(var.cluster_oidc_issuer_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "alb-controller-irsa"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+  role       = aws_iam_role.alb_controller_irsa.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+}
+
+resource "kubernetes_service_account" "alb_controller_sa" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+  }
+
+  automount_service_account_token = true
+
+  depends_on = [aws_iam_role.alb_controller_irsa]
+
+  provider = kubernetes.eks
+
+  annotations = {
+    "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller_irsa.arn
+  }
+}
