7e194fc744b7f70374440744775ac21386bfdfff
pcnpc128 <quasar421@gmail.com>
2025-06-18 10:07:11 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,13 +30,8 @@ variable "tokyo_public_subnets"   { default = ["10.2.1.0/24", "10.2.2.0/24"] }
 variable "tokyo_private_subnets"  { default = ["10.2.11.0/24", "10.2.12.0/24"] }
 variable "tokyo_azs"              { default = ["ap-northeast-1a", "ap-northeast-1c"] }
 
+# 추가된 변수들
 variable "region" {
-  description = "배포 대상 AWS 리전"
-  type        = string
-  default     = "ap-northeast-2"
-}
-
-variable "aws_region" {
   description = "배포 대상 AWS 리전"
   type        = string
   default     = "ap-northeast-2"
@@ -50,7 +45,7 @@ variable "cluster_name" {
 
 variable "vpc_id" {
   description = "EKS가 배포될 VPC ID"
-  type        = string
+  type        = map(string)
   default     = "default"
 }
 
@@ -64,12 +59,6 @@ variable "app_image" {
   description = "ECR에 저장된 애플리케이션 이미지"
   type        = string
   default     = "501257812675.dkr.ecr.ap-northeast-2.amazonaws.com/my-node-app:latest"
-}
-
-variable "app_service_name" {
-  description = "서비스 이름"
-  type        = string
-  default     = "default" # 임시 기본값
 }
 
 variable "db_host" {
@@ -106,4 +95,42 @@ variable "service_port" {
   description = "서비스가 외부에 노출할 포트"
   type        = number
   default     = 80
+}
+
+# Route53 연동용 추가 변수
+variable "seoul_rds_endpoint" {
+  description = "서울 RDS 엔드포인트 (FQDN 형식이어야 함)"
+  type        = string
+  default     = "mydb-seoul.cluster-abcdefghijk.ap-northeast-2.rds.amazonaws.com"
+}
+
+variable "tokyo_rds_endpoint" {
+  description = "도쿄 RDS 엔드포인트 (FQDN 형식이어야 함)"
+  type        = string
+  default     = "mydb-tokyo.cluster-abcdefghijk.ap-northeast-1.rds.amazonaws.com"
+}
+
+variable "domain" {
+  description = "Global DNS 이름 (예: www.abnormal-trust.click)"
+  type        = string
+  default     = "www.abnormal-trust.click"
+}
+
+variable "ga_dns" {
+  description = "Global Accelerator DNS 이름"
+  type        = string
+  default     = "a1b2c3d4e5f6g7.cloudfront.net"
+}
+
+# App 모듈 전달용
+variable "aws_region" {
+  description = "AWS 리전"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "app_service_name" {
+  description = "애플리케이션 서비스 이름"
+  type        = string
+  default     = "myapp-svc"
 }
