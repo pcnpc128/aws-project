d14b657f67899ad0f34825533f2b78dc7186cd5d
pcnpc128 <quasar421@gmail.com>
2025-06-18 11:38:31 +0900

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,20 +1,28 @@
-output "cluster_name"     { value = module.eks.cluster_name }
-output "cluster_endpoint" { value = module.eks.cluster_endpoint }
-output "cluster_ca"       { value = module.eks.cluster_certificate_authority_data }
-#output "oidc_provider_url" { value = module.eks.cluster_oidc_issuer_url }
-#output "oidc_thumbprint" { value = module.eks.cluster_oidc_thumbprint }
+output "cluster_name" {
+  description = "EKS 클러스터 이름"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS 클러스터 API 서버 엔드포인트"
+  value       = module.eks.cluster_endpoint
+}
+
 output "cluster_certificate_authority_data" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
+  description = "클러스터 인증서(base64)"
+  value       = module.eks.cluster_certificate_authority_data
 }
 
 output "cluster_oidc_issuer_url" {
-  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+  description = "EKS 클러스터의 OIDC Issuer URL"
+  value       = module.eks.oidc_provider
 }
 
 data "tls_certificate" "oidc_thumbprint" {
-  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+  url = module.eks.oidc_provider
 }
 
 output "cluster_oidc_thumbprint" {
-  value = data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint
+  description = "OIDC 프로바이더의 Thumbprint (IAM OIDC provider 생성 시 사용)"
+  value       = data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint
 }
