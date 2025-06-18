78d9dd485c71fc9ee70eea7bd49ebad8ad71ebb3
pcnpc128 <quasar421@gmail.com>
2025-06-18 11:44:25 +0900

    Update alb_irsa.tf

--- modules/eks-app/alb_irsa.tf
+++ modules/eks-app/alb_irsa.tf
@@ -41,15 +41,11 @@ resource "kubernetes_service_account" "alb_controller_sa" {
   metadata {
     name      = "aws-load-balancer-controller"
     namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller_irsa.arn
+    }
   }
-
   automount_service_account_token = true
-
   depends_on = [aws_iam_role.alb_controller_irsa]
-
   provider = kubernetes.eks
-
-  annotations = {
-    "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller_irsa.arn
-  }
 }
