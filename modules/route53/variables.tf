dd2fdcc7010b6a5de76481ff6f183b59e7ccbcfa
pcnpc128 <quasar421@gmail.com>
2025-06-18 17:20:34 +0900

    Update variables.tf

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -14,12 +14,6 @@ variable "domain" {
   type = string
 }
 
-variable "private_zone_id" {
-  description = "프라이빗 호스팅 존 ID"
-  type        = string
-  default     = "Z0AAAAAAFLTR38PKSEOUL" # 실제 값으로 대체
-}
-
 variable "ga_dns" {
   type = string
 }
