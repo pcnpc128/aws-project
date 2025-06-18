49a1b5cfcc6dac73b8dd213732b51fda1acd0050
pcnpc128 <quasar421@gmail.com>
2025-06-18 11:06:52 +0900

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -7,6 +7,38 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  alias = "seoul"
+  host                   = module.seoul_eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.seoul_eks.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.seoul.token
+}
+
+provider "helm" {
+  alias = "seoul"
+  kubernetes {
+    host                   = module.seoul_eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.seoul_eks.cluster_ca)
+    token                  = data.aws_eks_cluster_auth.seoul.token
+  }
+}
+
+provider "kubernetes" {
+  alias = "tokyo"
+  host                   = module.tokyo_eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.tokyo_eks.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.tokyo.token
+}
+
+provider "helm" {
+  alias = "tokyo"
+  kubernetes {
+    host                   = module.tokyo_eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.tokyo_eks.cluster_ca)
+    token                  = data.aws_eks_cluster_auth.tokyo.token
+  }
+}
+
 # AWS 공식 EKS 모듈 활용 (관리형 노드 그룹까지 자동 생성)
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
