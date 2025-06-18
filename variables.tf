776b4aec5002378b24301412b0c4e8ea5ad509c4
pcnpc128 <quasar421@gmail.com>
2025-06-18 09:53:18 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -42,10 +42,10 @@ variable "cluster_name" {
   default     = "myapp-seoul"
 }
 
-variable "vpc_id" {
-  description = "EKS가 배포될 VPC ID"
-  type        = string
-}
+#variable "vpc_id" {
+#  description = "EKS가 배포될 VPC ID"
+#  type        = string
+#}
 
 variable "app_name" {
   description = "애플리케이션 이름"
