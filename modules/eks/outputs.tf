c90257b95bacb5cd5d7f4bbe1ac0ea6cb9c71f70
pcnpc128 <quasar421@gmail.com>
2025-06-18 11:56:54 +0900

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,28 +1,28 @@
 output "cluster_name" {
   description = "EKS 클러스터 이름"
-  value       = module.eks.cluster_name
+  value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
   description = "EKS 클러스터 API 서버 엔드포인트"
-  value       = module.eks.cluster_endpoint
+  value = module.eks.cluster_endpoint
 }
 
-output "cluster_certificate_authority_data" {
+output "cluster_ca" {
   description = "클러스터 인증서(base64)"
-  value       = module.eks.cluster_certificate_authority_data
+  value = module.eks.cluster_certificate_authority[0].data
 }
 
 output "cluster_oidc_issuer_url" {
   description = "EKS 클러스터의 OIDC Issuer URL"
-  value       = module.eks.oidc_provider
+  value = module.eks.identity[0].oidc[0].issuer
 }
 
 data "tls_certificate" "oidc_thumbprint" {
-  url = module.eks.oidc_provider
+  url = module.eks.identity[0].oidc[0].issuer
 }
 
 output "cluster_oidc_thumbprint" {
   description = "OIDC 프로바이더의 Thumbprint (IAM OIDC provider 생성 시 사용)"
-  value       = data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint
+  value = data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint
 }
