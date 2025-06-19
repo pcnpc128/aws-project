31e8b3da4e97a1ce914ba4d1b78b782346b844b4
pcnpc128 <quasar421@gmail.com>
2025-06-19 12:08:11 +0900

    Update main.tf

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,7 +1,7 @@
 # 내부 RDS용 프라이빗 레코드 (모든 리전에서 접근 가능)
 
 resource "aws_route53_record" "rds_primary" {
-  zone_id = var.route53_zone_id
+  zone_id = var.private_zone_id
   name    = "sr.2whhosting.com"
   type    = "CNAME"
   ttl     = 30
@@ -9,7 +9,7 @@ resource "aws_route53_record" "rds_primary" {
 }
 
 resource "aws_route53_record" "rds_secondary" {
-  zone_id = var.route53_zone_id
+  zone_id = var.private_zone_id
   name    = "tr.2whhosting.com"
   type    = "CNAME"
   ttl     = 30
@@ -19,19 +19,19 @@ resource "aws_route53_record" "rds_secondary" {
 # VPC 연결 (서울/도쿄 모두 단일 프라이빗 존에 연결)
 resource "aws_route53_zone_association" "seoul_assoc" {
   vpc_id     = var.vpc_id["seoul"]
-  zone_id    = var.route53_zone_id
+  zone_id    = var.private_zone_id
   vpc_region = "ap-northeast-2"
 }
 
 resource "aws_route53_zone_association" "tokyo_assoc" {
   vpc_id     = var.vpc_id["tokyo"]
-  zone_id    = var.route53_zone_id
+  zone_id    = var.private_zone_id
   vpc_region = "ap-northeast-1"
 }
 
 # Global Accelerator와 연동된 전역 애플리케이션 DNS
 resource "aws_route53_record" "app_global_dns" {
-  zone_id = var.route53_zone_id
+  zone_id = var.public_zone_id
   name    = var.domain
   type    = "A"
 
