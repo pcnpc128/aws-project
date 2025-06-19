177845979edc882dd6520e8ffb2206cebb7d1b81
pcnpc128 <quasar421@gmail.com>
2025-06-19 09:36:59 +0900

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -12,6 +12,8 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
   subnet_ids      = var.public_subnets
   vpc_id          = var.vpc_id
 
