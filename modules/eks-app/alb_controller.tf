84a624eb223d4c9607475cd39fb6be08d0cf291d
pcnpc128 <quasar421@gmail.com>
2025-06-19 11:38:48 +0900

    Update alb_controller.tf

--- modules/eks-app/alb_controller.tf
+++ modules/eks-app/alb_controller.tf
@@ -5,35 +5,32 @@ resource "helm_release" "aws_load_balancer_controller" {
   namespace  = "kube-system"
   version    = "1.7.1"
 
-  depends_on = [kubernetes_service.app_svc]
+  depends_on = [kubernetes_service_account.alb_controller_sa]
 
-  set { # EKS 클러스터 이름
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-
-  set { # 서비스 계정 이름 (IRSA 구성 필요)
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-
-  set { # AWS 리전을 명시적으로 지정
-    name  = "region"
-    value = var.aws_region
-  }
-
-  set { # VPC ID 지정 (선택사항, 일부 기능 필요 시)
-    name  = "vpcId"
-    value = var.vpc_id
-  }
-
-  set {
-    name  = "image.repository"
-    value = "602401143452.dkr.ecr.${var.aws_region}.amazonaws.com/amazon/aws-load-balancer-controller"
-  }
-} 
+  set = [
+    {
+      name  = "clusterName"  # EKS 클러스터 이름
+      value = var.cluster_name
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    {
+      name  = "serviceAccount.create"  # 서비스 계정 이름 (IRSA 구성 필요)
+      value = "false"
+    },
+    {
+      name  = "region"  # AWS 리전을 명시적으로 지정
+      value = var.aws_region
+    },
+    {
+      name  = "vpcId"  # VPC ID 지정 (선택사항, 일부 기능 필요 시)
+      value = var.vpc_id
+    },
+    {
+      name  = "image.repository"
+      value = "602401143452.dkr.ecr.${var.aws_region}.amazonaws.com/amazon/aws-load-balancer-controller"
+    }
+  ]
+}
