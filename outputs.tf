c2472ea12280ca442121d4ca51cf8d8dc5af9f39
pcnpc128 <quasar421@gmail.com>
2025-06-25 16:18:18 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,14 @@ output "seoul_cluster_endpoint"  { value = module.seoul_eks.cluster_endpoint }
 output "tokyo_cluster_name"    { value = module.tokyo_eks.cluster_name }
 output "tokyo_cluster_endpoint"  { value = module.tokyo_eks.cluster_endpoint }
 
+# 서울 노드 그룹의 IAM Role 이름 출력
+output "seoul_node_group_iam_role_name" {
+  description = "IAM Role name for Seoul EKS node group"
+  value       = module.seoul_eks.eks_managed_node_groups["default"].iam_role_name
+}
+
+# 도쿄 노드 그룹의 IAM Role 이름 출력
+output "tokyo_node_group_iam_role_name" {
+  description = "IAM Role name for Tokyo EKS node group"
+  value       = module.tokyo_eks.eks_managed_node_groups["default"].iam_role_name
+}
